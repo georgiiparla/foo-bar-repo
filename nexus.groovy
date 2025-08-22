c4a9d9148094f053ae6453335db4013c98bf31fa
Georgii Parla <85189118+georgiiparla@users.noreply.github.com>
2025-08-22 14:58:19 +0200

    Update nexus.groovy

--- nexus.groovy
+++ nexus.groovy
@@ -1,41 +1,24 @@
 def publish(Map config) {
     script {
         def zipFile = config.zipFile
-        def repo = config.repoName
-        def nexusRepoUrl = "http://172.20.10.25:8081/repository/${repo}"
+        def nexusPath = config.nexusPath        // e.g., 'BASE/my-project/main'
         def credentialsId = config.credentialsId
 
-        def rawBranch = env.GIT_BRANCH
-        def currentBranch = rawBranch.replaceFirst('origin/', '')
-        def nexusPath
-        if (currentBranch == 'main') {
-            nexusPath = 'main'
-        } else {
-            nexusPath = currentBranch.replaceAll('/', '-')
-        }
-        
+        def nexusBaseUrl = "http://bo.rs2.com:8081"
+
         def versionIdentifier = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
         
-        def versionedUrl = "${nexusRepoUrl}/${nexusPath}/${versionIdentifier}/${zipFile}"
-        def latestUrl = "${nexusRepoUrl}/${nexusPath}/latest/${zipFile}"
+        def versionedUrl = "${nexusBaseUrl}/repository/${nexusPath}/${versionIdentifier}/${zipFile}"
+        def latestUrl = "${nexusBaseUrl}/repository/${nexusPath}/latest/${zipFile}"
 
         withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
             echo "Uploading VERSIONED artifact to: ${versionedUrl}"
-            sh """
-                curl -v -f -u "\$NEXUS_USER:\$NEXUS_PASS" \\
-                     --upload-file ${zipFile} \\
-                     "${versionedUrl}"
-            """
+            sh "curl -v -f -u \"\$NEXUS_USER:\$NEXUS_PASS\" --upload-file ${zipFile} \"${versionedUrl}\""
 
             echo "Uploading LATEST artifact to: ${latestUrl}"
-            sh """
-                curl -v -f -u "\$NEXUS_USER:\$NEXUS_PASS" \\
-                     --upload-file ${zipFile} \\
-                     "${latestUrl}"
-            """
+            sh "curl -v -f -u \"\$NEXUS_USER:\$NEXUS_PASS\" --upload-file ${zipFile} \"${latestUrl}\""
         }
         
-        echo "Cleaning up local file: ${zipFile}"
         sh "rm -f ${zipFile}"
     }
 }
@@ -43,14 +26,15 @@ def publish(Map config) {
 def download(Map config) {
     script {
         def zipFile = config.zipFile
-        def repo = config.repoPath
-        def nexusRepoUrl = "http://172.20.10.25:8081/repository/${repo}/${zipFile}"
+        def nexusPath = config.nexusPath
         def credentialsId = config.credentialsId
+        
+        def nexusBaseUrl = "http://bo.rs2.com:8081"
 
+        def fullDownloadUrl = "${nexusBaseUrl}/repository/${nexusPath}/${zipFile}"
+        
         withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'NEXUS_USER', passwordVariable: 'NEXUS_PASS')]) {
-            sh """
-                curl -v -f -u "\$NEXUS_USER:\$NEXUS_PASS" -o "${zipFile}" "${nexusRepoUrl}"
-            """
+            sh "curl -v -f -u \"\$NEXUS_USER:\$NEXUS_PASS\" -o \"${zipFile}\" \"${fullDownloadUrl}\""
         }
     }
 }
